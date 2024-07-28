a90cf7f6bf0fbe1efa4f9bae585beb1b3ba06b1d
Mishal Ibrahim <mishalrehan123@gmail.com>
2024-07-29 02:14:44 +0530

    search issue

--- app/(root)/editEnquiry/page.jsx
+++ app/(root)/editEnquiry/page.jsx
@@ -1,54 +1,58 @@
 'use client'
-import EnquiryForm from '@/components/EnquiryForm'
+import React, { useContext, useEffect, useState, Suspense } from 'react';
+import EnquiryForm from '@/components/EnquiryForm';
 import { Button } from '@/components/ui/button';
 import { AuthContext } from '@/context/authContext';
 import { getEnquiries } from '@/lib/services/api';
 import Link from 'next/link';
-import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useContext, useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation';
 
-const EditEnquiry = () => {
-  const { authState} = useContext(AuthContext);
+const EditEnquiryContent = () => {
+  const { authState } = useContext(AuthContext);
   const isLoggedIn = !!authState.accessToken;
-  const searchParams =useSearchParams();
+  const searchParams = useSearchParams();
   const [enquiry, setEnquiry] = useState(null);
-  let id = searchParams.get('id');
+  const id = searchParams.get('id');
 
   useEffect(() => {
-
-    const fetchEnquiry = async() =>{
-      if(id){
-        console.log(id,authState.accessToken)
+    const fetchEnquiry = async () => {
+      if (id) {
+        console.log(id, authState.accessToken);
         const response = await getEnquiries(authState.accessToken);
-              const data = response.data;
-              console.log({data})
-              const foundEnquiry = data.filter(enquiry => enquiry.enquiry_id == id);
-              console.log(foundEnquiry)
-              if (foundEnquiry) {
-                setEnquiry(foundEnquiry);
-              }
+        const data = response.data;
+        console.log({ data });
+        const foundEnquiry = data.find(enquiry => enquiry.enquiry_id == id);
+        console.log(foundEnquiry);
+        if (foundEnquiry) {
+          setEnquiry(foundEnquiry);
+        }
       }
-    }
+    };
     fetchEnquiry();
-  }, [ authState.accessToken]);
-
-  
+  }, [id, authState.accessToken]);
 
   return (
     <section className='pt-4 md:pt-10 md:px-[32px] px-2 flex w-full layout-height overflow-y-scroll no-scrollbar'>
-    {isLoggedIn ? (
-      <EnquiryForm type={'Edit'} accessToken ={authState.accessToken} enquiry={enquiry}/>
-    ) : (
-      <div className='flex flex-col gap-1.5'>
-      <p>You must be logged in to add an enquiry.</p>
-      <Button asChild>
-        <Link href='/sign-in'>Sign in</Link>
-      </Button>
-      </div>
-    )}
-  </section>
-);
+      {isLoggedIn ? (
+        <EnquiryForm type={'Edit'} accessToken={authState.accessToken} enquiry={enquiry} />
+      ) : (
+        <div className='flex flex-col gap-1.5'>
+          <p>You must be logged in to add an enquiry.</p>
+          <Button asChild>
+            <Link href='/sign-in'>Sign in</Link>
+          </Button>
+        </div>
+      )}
+    </section>
+  );
+};
 
-}
+const EditEnquiry = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditEnquiryContent />
+    </Suspense>
+  );
+};
 
-export default EditEnquiry;   
+export default EditEnquiry;
